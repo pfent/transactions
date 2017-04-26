98d2c2f86e89fa01d25dfc45ad368a1f9bdd5fa3
pfent <philipp@fent.de>
2017-04-26 09:09:40 +0200

    actually parse int / string, not just chars also comment the code, so correcting is easier :)

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 struct Operation {
     bool write;
     int transaction;
-    char field;
+    string field;
 
     /**
      * conflict matrix:
@@ -33,6 +33,9 @@ struct SRGraphNode {
     }
 };
 
+/**
+ * depth first search, detecting whether there are duplicates in the SRGraph, effectively detecting cycles in the graph
+ */
 bool dfsDuplicate(SRGraphNode *node, set<SRGraphNode *> visited = {}) {
     if (visited.find(node) != visited.end()) {
         return true;
@@ -44,35 +47,44 @@ bool dfsDuplicate(SRGraphNode *node, set<SRGraphNode *> visited = {}) {
 }
 
 int main(int argc, char *[]) {
-    vector<Operation> operations;
     int numberTransactions = 0;
-    for (char write, transaction, field;
-         cin >> write &&
-         cin >> transaction &&
-         cin >> field;) {
-        operations.push_back({write == 'w', transaction - '0', field});
-        if (operations.back().transaction > numberTransactions) {
-            numberTransactions = operations.back().transaction;
-        }
-    }
 
+    // initially read the individual operations as space separated 3-tuples
+    const auto operations = [&] {
+        vector<Operation> result;
+        char write;
+        int transaction;
+        string field;
+        while (cin >> write &&
+               cin >> transaction &&
+               cin >> field) {
+            result.push_back({write == 'w', transaction, field});
+            if (transaction > numberTransactions) {
+                numberTransactions = transaction;
+            }
+        }
+        return result;
+    }();
+
+    // initialize the SR graph
     vector<SRGraphNode> nodes;
     for (int i = 1; i <= numberTransactions; ++i) {
         nodes.push_back(SRGraphNode(i));
     }
 
+    // connect the conflicting transactions
     for (size_t i = 0; i < operations.size(); ++i) {
         const auto &op = operations[i];
+        // an operation can only conflict with later operations
         for (size_t j = i + 1; j < operations.size(); ++j) {
             const auto &other = operations[j];
             if (op.conflicts(other)) {
-                auto &conflict = nodes[other.transaction - 1];
-                nodes[op.transaction - 1].conflicts.push_back(&conflict);
+                nodes[op.transaction - 1].conflicts.push_back(&nodes[other.transaction - 1]);
             }
         }
     }
 
-    if (argc > 1) {
+    if (argc > 1) { // output DOT directed graph as a visualization
         cout << "digraph \"precedence graph\" {" << endl;
         for (const auto &node : nodes) {
             for (const auto &conflict : node.conflicts) {
@@ -82,7 +94,11 @@ int main(int argc, char *[]) {
         cout << "}" << endl;
     }
 
-    auto serializable = none_of(nodes.begin(), nodes.end(), [](SRGraphNode &node) {
+    // the whole history is serializable, iff the precedence graph has no cycles.
+    // note: we can't just start with any node, because the graph is not necessarily connected. We need to at least
+    // visit every node once. The most efficient implementation would only start at the next unvisited node again, but
+    // the least effort implementation just traverses the graph from every start node.
+    const auto serializable = none_of(nodes.begin(), nodes.end(), [](SRGraphNode &node) {
         return dfsDuplicate(&node);
     });
 
